4a7db5aea6382d8b7945ba5a1c765198d8069fe7
Yura B <yurkab24rs@gmail.com>
2022-08-16 15:11:06 +0300

    init: editing-users

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.scss';
+import { BrowserRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
